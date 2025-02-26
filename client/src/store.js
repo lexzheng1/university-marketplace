052f029fe02f3717af1d400bb885c0bf2cf980bc
lexzheng1 <128487569+lexzheng1@users.noreply.github.com>
2025-02-27 03:18:21 +0800

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
 // File: client/src/store.js
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
